f67da5ee1aacf69770d4005a84acc7affa3493a4
Vivek Mishra <v.mishra@f5.com>
2022-05-30 12:19:51 +0530

    registration form complete

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
+import {ReactiveFormsModule} from '@angular/forms'
 import {RouterModule} from '@angular/router'
 import {RegisterComponent} from 'src/app/auth/components/register/register.component'
 
@@ -10,7 +11,7 @@ const routes = [
   },
 ]
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
   declarations: [RegisterComponent],
 })
 export class AuthModule {}
